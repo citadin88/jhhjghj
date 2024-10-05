bb15fec8bf278d5c774eb7120a606fb426c1891c
citadin88 <31966923+citadin88@users.noreply.github.com>
2024-10-05 14:45:44 +0100

    Update and rename coin45.js to coin46.js

--- coin46.js
+++ coin46.js
@@ -49,7 +49,7 @@ function monitorAndCloseTab() {
                 } else {
                     console.log("URL contains '/c/'. Not closing the tab.");
                 }
-            }, 20000);
+            }, 30000);
 
             clearInterval(checkConditions);  // Stop the interval
         } else {
